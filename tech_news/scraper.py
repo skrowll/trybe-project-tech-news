ae83e8d16f5331fbf0f63ff299a98c7af8756d91
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-18 22:33:21 -0300

    feat(Req. 03): create function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,8 +25,9 @@ def scrape_updates(html_content: str) -> list:
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+def scrape_next_page_link(html_content: str) -> str:
+    selector = Selector(html_content)
+    return selector.css("a.next::attr(href)").get()
 
 
 # Requisito 4
