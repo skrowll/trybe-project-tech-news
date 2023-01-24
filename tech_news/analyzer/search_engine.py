58133c04349058def13d256bc656775c5377a21c
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-24 02:13:13 -0300

    feat(Req. 06): create function search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str) -> list:
+    query = {"title": {'$regex': title, "$options": 'i'}}
+    return [(news["title"], news["url"]) for news in search_news(query)]
 
 
 # Requisito 7
